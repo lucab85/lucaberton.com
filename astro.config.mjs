50cf1270786def761750a52b0d2d560753e41ea1
Luca Berton <luca@lucaberton.it>
2025-03-04 15:55:33 +0100

    GA4

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,17 @@ import tailwindcss from "@tailwindcss/vite";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
+import googleAnalytics from "astro-google-analytics";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://lucaberton.com",
   base: "/",
-  integrations: [mdx(), sitemap(), icon()],
+  integrations: [mdx(), sitemap(), icon(),
+    googleAnalytics({
+      measurementId: "G-M9Q5F672JT",
+    }),
+  ],
   vite: {
     plugins: [tailwindcss()],
   },

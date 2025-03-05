1f66a9e53a28812be32117e629330ccd941e2982
Luca Berton <luca@lucaberton.it>
2025-03-05 14:35:02 +0100

    "prettier": "^3.5.3"

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,6 +14,17 @@ export default defineConfig({
     sitemap(),
     icon(),
     partytown({ config: { forward: ["dataLayer.push"] } }), // Using the correct import
+    (await import("astro-compress")).default({
+      CSS: true,  // Astro-compress for minify
+      HTML: {
+        "html-minifier-terser": {
+          removeAttributeQuotes: true,
+        },
+      },
+      Image: false,
+      JavaScript: true,
+      SVG: true,
+    }),
   ],
   vite: {
     plugins: [tailwindcss()],

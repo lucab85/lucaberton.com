08c9258cfb7f987146c65388b72ddfcf3f07b820
Luca Berton <luca@lucaberton.it>
2025-03-19 12:42:40 +0100

    Blog

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,1 +1,5 @@
-export default new Map();
+
+export default new Map([
+["src/content/blog/Ansible for VMware by Examples.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2FAnsible+for+VMware+by+Examples.mdx&astroContentModuleFlag=true")],
+["src/content/blog/Ansible for Kubernetes by Example.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2FAnsible+for+Kubernetes+by+Example.mdx&astroContentModuleFlag=true")]]);
+		

47cf721367ec4ff7c5b49e9ea5897c6df61453a0
Luca Berton <luca@lucaberton.it>
2026-02-03 00:31:30 +0100

    SEO

--- src/components/Category.tsx
+++ src/components/Category.tsx
@@ -14,13 +14,13 @@ function Category({ title, lastPost }: CategoryProps) {
   return (
     <li className="my-6">
       <a
-        href={`/blog/categories/${slugify(title, { lower: true, strict: true })}`}
+        href={`/blog/categories/${slugify(title, { lower: true, strict: true })}/`}
         className="text-skin-accent inline-block text-lg font-medium decoration-dashed underline-offset-4 focus-visible:no-underline focus-visible:underline-offset-0"
       >
         <h2>{title}</h2>
       </a>
       <div className="my-1 flex gap-1">
-        <a href={`/blog/${lastPost?.slug}`} className="text-sm">
+        <a href={`/blog/${lastPost?.slug}/`} className="text-sm">
           <b>Latest post</b>: {lastPost?.data.title}
         </a>
       </div>
